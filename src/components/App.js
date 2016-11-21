cf16c5008449926f549a5e5da11a3f541af67602
Buck <buck9063@gmail.com>
2016-11-21 11:11:31 -0800

    Create App initial state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,17 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+    // initial state
+    this.state = {
+      fishes: {},
+      order: {},
+    };
+  }
+
   render() {
     return(
       <div className="catch-of-the-day">
@@ -13,9 +24,9 @@ class App extends React.Component {
         <Order />
         <Inventory />
       </div>
-
     )
   }
+
 }
 
 export default App;
