a18cb6075b4d8a139cd429c988229e8cf1902a50
Buck <buck9063@gmail.com>
2016-11-21 11:41:14 -0800

    Propagate user-added fish to App state

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,34 @@ class App extends React.Component {
   constructor() {
     super();
 
+    this.addFish = this.addFish.bind(this);
+
     // initial state
     this.state = {
       fishes: {},
       order: {},
     };
+
+  }
+
+  addFish(fish) {
+
+    // update our state
+
+    // Could just do:
+    // this.state.fishes.fish1 = fish;
+    // but this is not considered good practice
+    // best practice: first make copy of state
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+
+    // set state
+    // Not all state, just the 'fishes' part that we updated
+    // take the new local const 'fishes' and set it to be the new App state for 'fishes'
+    this.setState({fishes: fishes});
+    // alternative shortcut: this.setState({ fishes });
+
   }
 
   render() {
@@ -22,7 +45,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
