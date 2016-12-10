07daec169a65df2e1791a9106bfde69cbe6c961e
Buck <buck9063@gmail.com>
2016-12-10 15:35:34 -0800

    Add Firebase database persistence and realtime syncing.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
-import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
 
@@ -20,6 +21,18 @@ class App extends React.Component {
       order: {},
     };
 
+  }
+
+  componentWillMount(){
+    this.ref = base.syncState(`${this.props.params.storeID}/fishes`,
+    {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount(){
+    base.removeBinding(this.ref);
   }
 
   addFish(fish) {
