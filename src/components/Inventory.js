663eda5ba7cfa1b45e90f2ef9e74b6edb5c41faf
Buck <buck9063@gmail.com>
2016-12-11 18:02:20 -0800

    Dynamically display placeholders in Inventory for all fishes.

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,10 +2,20 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
+
+  renderInventory(key) {
+    return (
+      <p>{key}</p>
+    )
+  }
+
   render() {
     return(
       <div>
         <h2>Inventory</h2>
+        {/* Loop through each fish to display it as inventory */}
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
+
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
